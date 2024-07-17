f60c1b38732b8f93f2447f7293fd6bcf1ff4c803
DarkXamper <krishguptano12@gmail.com>
2024-07-17 23:21:12 +0530

    add @vercel/speed-insights for tracking website analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 // import App from './App.jsx'
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router.jsx";
+injectSpeedInsights();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
