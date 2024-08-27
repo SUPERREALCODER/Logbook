d6cb8e845c60b39c40f807c758afa022ef3e4767
DarkXamper <krishguptano12@gmail.com>
2024-08-27 22:10:43 +0530

    add:added contact feature

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,28 @@
 const Contact = () => {
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    formData.append("access_key", import.meta.env.VITE_CONTACT_ACCESS_KEY);
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    }).then((res) => res.json());
+
+    if (res.success) {
+      // return <ThankYouMessage />;
+      console.log("Success");
+    }
+  };
+
   return (
     <>
-      {/* <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-100">
-      <h1 className="text-7xl">Contact USSSSSSSS</h1>
-    </div> */}
       <div className="flex flex-col items-center min-h-screen py-10 dark:bg-[#0d121cf9]">
         <div className="max-w-7xl w-full px-4 sm:px-6 lg:px-8">
           <div className="text-center">
@@ -18,7 +37,7 @@ const Contact = () => {
               products.
             </p>
           </div>
-          <div>
+          <form onSubmit={onSubmit}>
             <div className="flex flex-col md:flex-row items-center justify-center md:space-x-6 md:space-y-0 space-y-6 mt-10">
               <div className="w-full px-4 py-6 space-y-2 bg-white dark:bg-gray-900 rounded-lg shadow-md border dark:border-gray-600">
                 <h4 className="text-lg font-semibold text-gray-900 dark:text-white">
@@ -26,9 +45,11 @@ const Contact = () => {
                 </h4>
                 <div className=" w-full">
                   <input
-                    type="text"
+                    type="email"
+                    name="email"
                     className="w-full p-4 text-gray-700 placeholder:dark:text-gray-400 outline-none rounded-lg border-2 dark:border-gray-600 dark:bg-gray-800"
                     placeholder="Enter your email"
+                    required
                   />
                 </div>
                 <h4 className="text-lg font-semibold text-gray-900 dark:text-white">
@@ -40,6 +61,7 @@ const Contact = () => {
                     type="text"
                     className="w-full p-4 rounded-lg text-gray-700 border-2 dark:border-gray-600 dark:bg-gray-800 outline-none placeholder:dark:text-gray-400"
                     placeholder="Enter your message"
+                    name="description"
                   />
                 </div>
                 <button
@@ -50,7 +72,7 @@ const Contact = () => {
                 </button>
               </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </>
@@ -58,3 +80,18 @@ const Contact = () => {
 };
 
 export default Contact;
+
+// function ThankYouMessage() {
+//   return (
+//     <div className="bg-[#e7eaf0] dark:bg-gray-900 p-10 rounded-lg shadow-lg text-center max-w-screen-xl mx-auto">
+//       <h1 className="text-6xl font-bold text-green-600 dark:text-green-400">
+//         Success!
+//       </h1>
+//       <p className="mt-2 text-gray-600 dark:text-gray-400 text-xl max-w-prose mx-auto">
+//         {
+//           "Your message has been sent successfully, we will get back to you soon."
+//         }
+//       </p>
+//     </div>
+//   );
+// }
